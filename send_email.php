12bfa056166585845e30d8072f110dd0f7e30ddd
luwes <luismayrinaa@gmail.com>
2024-05-21 17:24:28 +0800

    update

--- send_email.php
+++ send_email.php
@@ -10,7 +10,7 @@ require 'phpmailer/src/SMTP.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['send'])) {
     $name = ($_POST['name']);
     $email = ($_POST['email']);
     $message = ($_POST['message']);
@@ -23,23 +23,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();                           // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';      // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                  // Enable SMTP authentication
-        $mail->Username   = 'pirepath2024@gmail.com'; // SMTP username
+        $mail->Username   = 'piratepath2024@gmail.com'; // SMTP username
         $mail->Password   = 'eaqwslugoqqnqxec';    // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption, `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 587;                   // TCP port to connect to
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption, `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 465;                   // TCP port to connect to
 
         //Recipients
-        $mail->setFrom('pirepath2024@gmail.com');
-        $mail->addAddress('pirepath2024@gmail.com'); // Add a recipient
+        $mail->setFrom('piratepath2024@gmail.com', 'PiratePath');
+        $mail->addAddress('luismayrinaa@gmail.com', 'Luis Mayrina'); // Add a recipient
 
         // Content
         $mail->isHTML(true);                       // Set email format to HTML
         $mail->Subject = 'PiratePath Contact Form: ' . $subject;
-        $mail->Body    = "<h2>Contact Us Form Submission</h2>
-                          <p><strong>Name:</strong> {$name}</p>
-                          <p><strong>Email:</strong> {$email}</p>
-                          <p><strong>Message:</strong> {$message}</p>";
-      //  $mail->AltBody = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
+        $mail->Body    = "<h2>Contact Us Form Submission</h2>";
+        $mail->AltBody = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
 
         $mail->send();
         echo 'Message has been sent';
